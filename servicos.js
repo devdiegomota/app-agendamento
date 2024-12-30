2ffd949bcec2c507aa33a69600e6f52c3f943cd5
Diego Mota <diego.motasocial@gmail.com>
2024-12-30 00:04:47 -0300

    aa

---
+++ servicos.js
@@ -0,0 +1,24 @@
+function toggleDropdown() {
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.classList.toggle('active');
+  }
+
+  function toggleDropdown2() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns[1].classList.toggle('active');
+  }
+
+  function toggleDropdown3() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns[2].classList.toggle('active');
+  }
+
+  function toggleDropdown4() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns[3].classList.toggle('active');
+  }
+
+  function toggleDropdown5() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns[4].classList.toggle('active');
+  }
